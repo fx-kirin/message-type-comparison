203291c09ceec1424a4bb7458c54d02f1a2bccc6
fx_kirin <fx.kirin@gmail.com>
2020-01-06 16:51:45 +0900

    fix

--- message_type_comparison/__main__.py
+++ message_type_comparison/__main__.py
@@ -22,29 +22,66 @@ from benchmarker import Benchmarker
 import cbor2
 import kanilog
 import stdlogging
+import orjson
+
+
+def default(obj):
+    if isinstance(obj, np.ndarray):
+        return obj.tolist()
 
 
 def main():
     logging.info("getting benchmark")
     a = np.arange(100000.0)
-    a = list(a)
+    listed = a.tolist()
+
     with Benchmarker(100000, width=50) as bench:
 
         @bench("cbor2.dump")
         def _(bm):
-            data = cbor2.dumps(list(a))
+            data = cbor2.dumps(listed)
             cbor2.loads(data)
 
         @bench("message_pack")
         def _(bm):
-            data = msgpack.packb(list(a))
+            data = msgpack.packb(listed)
             msgpack.unpackb(data)
+
+        @bench("orjson")
+        def _(bm):
+            data = orjson.dumps(listed)
+            orjson.loads(data)
 
         for i in range(5):
 
             @bench("pickle %s" % (i))
             def _(bm):
-                data = pickle.dumps(list(a), protocol=i)
+                data = pickle.dumps(a, protocol=i)
+                pickle.loads(data)
+
+    a = list([float(v) for v in a])
+    with Benchmarker(100000, width=50) as bench:
+
+        @bench("cbor2.dump")
+        def _(bm):
+            data = cbor2.dumps(a)
+            cbor2.loads(data)
+
+        @bench("message_pack")
+        def _(bm):
+            data = msgpack.packb(a)
+            msgpack.unpackb(data)
+
+        @bench("orjson")
+        def _(bm):
+            data = orjson.dumps(a)
+            orjson.loads(data)
+
+        for i in range(5):
+
+            @bench("pickle %s" % (i))
+            def _(bm):
+                data = pickle.dumps(a, protocol=i)
                 pickle.loads(data)
 
 
